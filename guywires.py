c4c84ef43765f80dafe9354efae5580bbf62be16
Jose M. Albarrán <jmalbarran@b-thinking.com>
2025-10-21 21:30:18 +0200

    Adjusted types to float Added locale management for input and output

--- guywires.py
+++ guywires.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 import os
 import logging
 import numpy as np
+import locale
+from typing import Any
 
 # region: Initialize environment
 load_dotenv()
@@ -15,38 +17,22 @@ logging.basicConfig(
 
 logger.info(f"Guy Wires - Logging level: {logger.getEffectiveLevel()}")
 
+# Initialize locale using current environment
+locale.setlocale(locale.LC_ALL, '')
+
+curlocale: dict[str, Any] = dict(locale.localeconv())
+list_sep = ';' if curlocale['decimal_point'] == ',' else ','
 # endregion
-
-
-# region: Input parameters
-def get_input_parameters_int(param_name: str, default_value: 0) -> int:
-    param_str = input(f"{param_name} [{default_value}]: ")
-    try:
-        return int(param_str)
-    except ValueError:
-        return default_value
-
-
-wind_presure = get_input_parameters_int("Wind pressure (N)", 100)
-# wind_angle = get_input_parameters_int("Wind angle (deg)", 30)
-guy_wires_angles: list[int] = [0, 120, 240]
-i: int
-angle: int
-for i, angle in enumerate(guy_wires_angles):
-    guy_wires_angles[i] = get_input_parameters_int(f"Guy wires angle {i} (deg)", angle)
-
-# endregion: Input parameters
-
 
 # region: Tensions
 def get_tensions(
-    wind_presure: int, wind_angle: int, guy_wires_angles: list[int], tolerance: float = 1e-9
+    wind_presure: float, wind_angle: float, guy_wires_angles: list[float], tolerance: float = 1e-9
 ) -> list[float]:
     """
     Get tensions for each guy wire for a given wind pressure and angle
 
     :param wind_presure: Wind pressure in Newtons
-    :param wind_angle: Wind angle in degrees
+    :param wind_angle: Wind angle in sexagesimal degrees
     :param guy_wires_angles: List of guy wires angles in degrees
     :return tensions: List of tensions for each guy wire
     """
@@ -82,19 +68,21 @@ def get_tensions(
             return tensions.tolist()
         U_actives: np.ndarray = U[active_wires, :]
 
+        T_actives: np.ndarray
         if active_wires_count >= 2: # At least 2 active wires
             try:
                 # Solve linear system U_actives^T * tensions = -F
                 M = np.linalg.inv(U_actives.T @ U_actives)  # 2x2 (U^T·U)^−1
                 # invM = np.linalg.inv(M)
-                T_actives = - (U_actives @ (M @ F))
+                T_actives: np.ndarray  = - (U_actives @ (M @ F))
             except np.linalg.LinAlgError:
-                # No direct solution, approximate with least squares                
-                T_actives = np.linalg.lstsq(U_actives, -F, rcond=None)
+                # No direct solution, approximate with least squares 
+                logger.warning("No direct solution, approximate with least squares. THIS CANNOT HAPPEN WITH ONLY 3 WIRES")               
+                T_actives, _, _, _  = np.linalg.lstsq(U_actives, -F, rcond=None)
         else:
             # Only 1 active wire. Project F on it
             T_value = max(0.0, - np.dot(F, U_actives[0]))
-            T_actives = np.array([T_value])
+            T_activesy = np.array([T_value])
 
 
         # If any tension is negative, remove active wire and recalculate
@@ -116,15 +104,47 @@ def get_tensions(
 
 # endregion: Tensions
 
+
+# region: Input parameters
+def get_input_parameters_float(param_name: str, default_value: float = 0.0) -> float:
+    param_str = input(f"{param_name} [{locale.format_string('%.1f',default_value,grouping=True)}]: ")
+    try:
+        return locale.atof(param_str)
+    except ValueError:
+        return default_value
+
+
+wind_presure = get_input_parameters_float("Wind pressure (N)", 100.0)
+# wind_angle = get_input_parameters_int("Wind angle (deg)", 30)
+guy_wires_angles: list[float] = [0.0, 120.0, 240.0]
+i: int
+angle: float
+prev_angle: float = 0.0
+for i, angle in enumerate(guy_wires_angles):
+    while True:
+        guy_wires_angles[i] = get_input_parameters_float(f"Guy wires heading angle {i} (deg)", angle)
+        if guy_wires_angles[i] >= prev_angle and guy_wires_angles[i] <= 360.0:
+            prev_angle = guy_wires_angles[i]
+            break
+        else:
+            print(f"Invalid angle. It has to be between {prev_angle} and 360 degrees. Try again.")
+
+# endregion: Input parameters
+
+
+
 # region: Output
 
-print(f"Wind pressure: {wind_presure} N")
-print(f"Guy wires angles: {guy_wires_angles}")
-print("Angle,  Tension 1, Tension 2, Tension 3,")
-for angle in range(0, 360, 10):
+print(f"Wind pressure: {locale.format_string('%3.0f',wind_presure, grouping=True)} N")
+print(f"Guy wires angles: ", end="")
+for angle in guy_wires_angles:
+    print(f"{locale.format_string('%.1f',angle,grouping=True)}{list_sep}", end="")
+print("")
+print(f"Angle{list_sep}  Tension 1{list_sep} Tension 2{list_sep} Tension 3{list_sep}")
+for angle in np.arange(0.0, 360.0, 10.0):
     tensions = get_tensions(wind_presure, angle, guy_wires_angles)
-    print(f"  {angle:3.0f}, ", end="")
+    print(f"  {locale.format_string('%3.0f',angle,grouping=True)}{list_sep} ", end="")
     for tension in tensions:
-        print(f"{tension:10.2f},", end="")
+        print(f"{locale.format_string('%10.1f',tension,grouping=True)}{list_sep}", end="")
     print("")
 # endregion
